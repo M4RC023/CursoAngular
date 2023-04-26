dbb74833b3ef264986cc28534b179f7419cb8486
M4RC023 <marco.anthonio23@gmail.com>
2023-04-26 14:10:24 -0300

    Seção 6: Multiplas camadas

---
+++ Back/src/ProEventos.Persistence/ProEventosPersistence.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.Persistence 
+{
+    public class ProEventosPersistence : IProEventosPersistence
+    {
+        public void Add <T> (T entity) where T :class
+        {
+            
+        }
+    }
+} 
